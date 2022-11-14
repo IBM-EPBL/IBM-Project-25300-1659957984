80ad58feb7af9fa23f535393c31f491e30f91bfc
manisha18821 <85789531+manisha18821@users.noreply.github.com>
2022-11-14 16:02:50 +0530

    Add files for Sprint 3

--- Project Development/Sprint 3/Code/app.py
+++ Project Development/Sprint 3/Code/app.py
@@ -18,6 +18,30 @@ def main():
 def dashboard():
     return render_template("dashboard.html")
 
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    if request.method == "POST":
+        # Get values through input bars
+        name = request.form.get("name")
+        age =   request.form.get('age')
+        sex =   request.form.get('sex')
+        cp =   request.form.get('cp')
+        trestbps =   request.form.get('trestbps')
+        chol =   request.form.get('chol')
+        fbs =   request.form.get('fbs')
+        restecg =   request.form.get('restecg')
+        thalach =   request.form.get('thalach')
+        exang =   request.form.get('exang')
+        oldpeak =   request.form.get('oldpeak')
+        slope =   request.form.get('slope')
+        ca =   request.form.get('ca')
+        thal =   request.form.get('thal')
+        
+        user_input = [age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]
+
+        print(age,sex,cp,trestbps,restecg,chol,fbs,thalach,exang,oldpeak,slope,ca,thal)
+    return render_template("predictor.html")
+
 # Running the app
 if __name__ == '__main__':
     app.run(debug = True)
